32fef084a68ddf6da7165bd35b2c8c42a40fbf17
Hak1e <hakie.notiko@gmail.com>
2025-01-14 20:49:23 +0400

    Working grade analysis

--- University-Dasboard/FrmGradeRecords.Designer.cs
+++ University-Dasboard/FrmGradeRecords.Designer.cs
@@ -33,13 +33,15 @@
 			npgsqlDataAdapter1 = new Npgsql.NpgsqlDataAdapter();
 			fpGradeAnalysis = new ScottPlot.WinForms.FormsPlot();
 			fpGradePrediction = new ScottPlot.WinForms.FormsPlot();
-			dtpFrom = new DateTimePicker();
-			dtpBy = new DateTimePicker();
+			dtpPeriodFrom = new DateTimePicker();
+			dtpPeriodBy = new DateTimePicker();
 			label2 = new Label();
 			label1 = new Label();
 			label3 = new Label();
-			cbFaculty = new ComboBox();
 			label4 = new Label();
+			cbFaculty = new ComboBox();
+			lbSelectedFaculty = new Label();
+			btnShow = new Button();
 			((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
 			SuspendLayout();
 			// 
@@ -53,47 +55,47 @@
 			// fpGradeAnalysis
 			// 
 			fpGradeAnalysis.DisplayScale = 2F;
-			fpGradeAnalysis.Location = new Point(62, 194);
+			fpGradeAnalysis.Location = new Point(12, 219);
 			fpGradeAnalysis.Name = "fpGradeAnalysis";
-			fpGradeAnalysis.Size = new Size(356, 328);
+			fpGradeAnalysis.Size = new Size(450, 422);
 			fpGradeAnalysis.TabIndex = 0;
 			// 
 			// fpGradePrediction
 			// 
 			fpGradePrediction.DisplayScale = 1F;
-			fpGradePrediction.Location = new Point(588, 194);
+			fpGradePrediction.Location = new Point(518, 219);
 			fpGradePrediction.Name = "fpGradePrediction";
-			fpGradePrediction.Size = new Size(356, 328);
+			fpGradePrediction.Size = new Size(450, 422);
 			fpGradePrediction.TabIndex = 0;
 			// 
-			// dtpFrom
+			// dtpPeriodFrom
 			// 
-			dtpFrom.CustomFormat = "MMMM yyyy";
-			dtpFrom.Format = DateTimePickerFormat.Custom;
-			dtpFrom.Location = new Point(373, 90);
-			dtpFrom.Name = "dtpFrom";
-			dtpFrom.ShowCheckBox = true;
-			dtpFrom.ShowUpDown = true;
-			dtpFrom.Size = new Size(127, 23);
-			dtpFrom.TabIndex = 55;
+			dtpPeriodFrom.CustomFormat = "MMMM yyyy";
+			dtpPeriodFrom.Format = DateTimePickerFormat.Custom;
+			dtpPeriodFrom.Location = new Point(12, 73);
+			dtpPeriodFrom.Name = "dtpPeriodFrom";
+			dtpPeriodFrom.ShowCheckBox = true;
+			dtpPeriodFrom.ShowUpDown = true;
+			dtpPeriodFrom.Size = new Size(127, 23);
+			dtpPeriodFrom.TabIndex = 55;
 			// 
-			// dtpBy
+			// dtpPeriodBy
 			// 
-			dtpBy.CustomFormat = "MMMM yyyy";
-			dtpBy.Format = DateTimePickerFormat.Custom;
-			dtpBy.Location = new Point(506, 90);
-			dtpBy.Name = "dtpBy";
-			dtpBy.ShowCheckBox = true;
-			dtpBy.ShowUpDown = true;
-			dtpBy.Size = new Size(127, 23);
-			dtpBy.TabIndex = 55;
+			dtpPeriodBy.CustomFormat = "MMMM yyyy";
+			dtpPeriodBy.Format = DateTimePickerFormat.Custom;
+			dtpPeriodBy.Location = new Point(145, 73);
+			dtpPeriodBy.Name = "dtpPeriodBy";
+			dtpPeriodBy.ShowCheckBox = true;
+			dtpPeriodBy.ShowUpDown = true;
+			dtpPeriodBy.Size = new Size(127, 23);
+			dtpPeriodBy.TabIndex = 55;
 			// 
 			// label2
 			// 
 			label2.AutoSize = true;
 			label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
 			label2.ForeColor = Color.FromArgb(158, 161, 178);
-			label2.Location = new Point(425, 67);
+			label2.Location = new Point(64, 50);
 			label2.Name = "label2";
 			label2.Size = new Size(26, 20);
 			label2.TabIndex = 56;
@@ -104,7 +106,7 @@
 			label1.AutoSize = true;
 			label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
 			label1.ForeColor = Color.FromArgb(158, 161, 178);
-			label1.Location = new Point(552, 67);
+			label1.Location = new Point(191, 50);
 			label1.Name = "label1";
 			label1.Size = new Size(37, 20);
 			label1.TabIndex = 57;
@@ -115,11 +117,22 @@
 			label3.AutoSize = true;
 			label3.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
 			label3.ForeColor = Color.FromArgb(158, 161, 178);
-			label3.Location = new Point(373, 26);
+			label3.Location = new Point(12, 9);
 			label3.Name = "label3";
 			label3.Size = new Size(260, 31);
 			label3.TabIndex = 56;
 			label3.Text = "Выберите период:";
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Bold);
+			label4.ForeColor = Color.FromArgb(158, 161, 178);
+			label4.Location = new Point(313, 9);
+			label4.Name = "label4";
+			label4.Size = new Size(166, 31);
+			label4.TabIndex = 58;
+			label4.Text = "Факультет:";
 			// 
 			// cbFaculty
 			// 
@@ -128,21 +141,37 @@
 			cbFaculty.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold);
 			cbFaculty.ForeColor = Color.FromArgb(24, 30, 54);
 			cbFaculty.FormattingEnabled = true;
-			cbFaculty.Location = new Point(407, 152);
+			cbFaculty.Location = new Point(295, 70);
 			cbFaculty.Name = "cbFaculty";
 			cbFaculty.Size = new Size(209, 26);
 			cbFaculty.TabIndex = 59;
+			cbFaculty.SelectedIndexChanged += cbFaculty_SelectedIndexChanged;
 			// 
-			// label4
+			// lbSelectedFaculty
 			// 
-			label4.AutoSize = true;
-			label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-			label4.ForeColor = Color.FromArgb(158, 161, 178);
-			label4.Location = new Point(447, 129);
-			label4.Name = "label4";
-			label4.Size = new Size(108, 20);
-			label4.TabIndex = 58;
-			label4.Text = "Факультет:";
+			lbSelectedFaculty.AutoSize = true;
+			lbSelectedFaculty.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			lbSelectedFaculty.ForeColor = Color.FromArgb(158, 161, 178);
+			lbSelectedFaculty.Location = new Point(24, 187);
+			lbSelectedFaculty.Name = "lbSelectedFaculty";
+			lbSelectedFaculty.Size = new Size(421, 29);
+			lbSelectedFaculty.TabIndex = 58;
+			lbSelectedFaculty.Text = "Анализ успеваемости за период";
+			// 
+			// btnShow
+			// 
+			btnShow.BackColor = Color.FromArgb(0, 126, 249);
+			btnShow.FlatAppearance.BorderSize = 0;
+			btnShow.FlatStyle = FlatStyle.Flat;
+			btnShow.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btnShow.ForeColor = Color.FromArgb(24, 30, 54);
+			btnShow.Location = new Point(545, 52);
+			btnShow.Name = "btnShow";
+			btnShow.Size = new Size(120, 44);
+			btnShow.TabIndex = 60;
+			btnShow.Text = "Посмотреть";
+			btnShow.UseVisualStyleBackColor = false;
+			btnShow.Click += btnShow_Click;
 			// 
 			// FrmGradeRecords
 			// 
@@ -150,13 +179,15 @@
 			AutoScaleMode = AutoScaleMode.Font;
 			BackColor = Color.FromArgb(46, 51, 73);
 			ClientSize = new Size(980, 653);
+			Controls.Add(btnShow);
 			Controls.Add(cbFaculty);
+			Controls.Add(lbSelectedFaculty);
 			Controls.Add(label4);
 			Controls.Add(label1);
 			Controls.Add(label3);
 			Controls.Add(label2);
-			Controls.Add(dtpBy);
-			Controls.Add(dtpFrom);
+			Controls.Add(dtpPeriodBy);
+			Controls.Add(dtpPeriodFrom);
 			Controls.Add(fpGradePrediction);
 			Controls.Add(fpGradeAnalysis);
 			FormBorderStyle = FormBorderStyle.None;
@@ -172,12 +203,14 @@
 		private Npgsql.NpgsqlDataAdapter npgsqlDataAdapter1;
 		private ScottPlot.WinForms.FormsPlot fpGradeAnalysis;
 		private ScottPlot.WinForms.FormsPlot fpGradePrediction;
-		private DateTimePicker dtpFrom;
-		private DateTimePicker dtpBy;
+		private DateTimePicker dtpPeriodFrom;
+		private DateTimePicker dtpPeriodBy;
 		private Label label2;
 		private Label label1;
 		private Label label3;
-		private ComboBox cbFaculty;
 		private Label label4;
+		private ComboBox cbFaculty;
+		private Label lbSelectedFaculty;
+		private Button btnShow;
 	}
 }
--- University-Dasboard/FrmGradeRecords.cs
+++ University-Dasboard/FrmGradeRecords.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data;
+using Database;
 using ScottPlot;
+using University_Dasboard.Database.Models;
 
 namespace University_Dasboard
 {
@@ -16,29 +10,80 @@ namespace University_Dasboard
 		public FrmGradeRecords()
 		{
 			InitializeComponent();
-			LoadGradeAnalysis();
+			LoadData();
 		}
+		private Faculty? selectedFaculty;
 
+		private void LoadData()
+		{
+			using var ctx = new DatabaseContext();
+			var faculties = ctx.Faculty.ToList();
+			ComboboxHelper.LoadCombobox(
+				faculties,
+				comboBox: cbFaculty);
+		}
 		private void LoadGradeAnalysis()
 		{
+			using var ctx = new DatabaseContext();
+			if (selectedFaculty == null)
+			{
+				return;
+			}
+			var marks = ctx.Marks
+				.Where(m => m.GradeDate > dtpPeriodFrom.Value.Date.ToUniversalTime() // условие по дате
+				&& m.GradeDate <= dtpPeriodBy.Value.Date.ToUniversalTime()) // продолжение условия
+				.Where(m => m.Student!.Group!.Direction!.FacultyId == selectedFaculty.Id) // где объект Оценка имеет id выбранного факультета
+				.ToList();
+
+			int mark5 = marks.Count(m => m.Mark == 5);
+			int mark4 = marks.Count(m => m.Mark == 4);
+			int mark3 = marks.Count(m => m.Mark == 3);
+			int mark2 = marks.Count(m => m.Mark == 2);
+			int mark1 = marks.Count(m => m.Mark == 1);
+			int totalMarks = mark5 + mark4 + mark3 + mark2 + mark1;
+			double percent5 = (double)mark5 / totalMarks * 100;
+			double percent4 = (double)mark4 / totalMarks * 100;
+			double percent3 = (double)mark3 / totalMarks * 100;
+			double percent2 = (double)mark2 / totalMarks * 100;
+			double percent1 = (double)mark1 / totalMarks * 100;
+
+			// Создание частей круга
 			List<PieSlice> slices = new()
 			{
-				new PieSlice() { Value = 5, FillColor = Colors.Red, Label = "Red" },
-				new PieSlice() { Value = 2, FillColor = Colors.Orange, Label = "Orange" },
-				new PieSlice() { Value = 8, FillColor = Colors.Gold, Label = "Yellow" },
-				new PieSlice() { Value = 4, FillColor = Colors.Green, Label = "Green" },
-				new PieSlice() { Value = 8, FillColor = Colors.Blue, Label = "Blue" },
+				new PieSlice() { Value = mark5, 
+					FillColor = Colors.Green, Label = $"5 баллов ({percent5:F2}%)" },
+				new PieSlice() { Value = mark4, 
+					FillColor = Colors.GreenYellow, Label = $"4 балла ({percent4:F2}%)" },
+				new PieSlice() { Value = mark3, 
+					FillColor = Colors.Yellow, Label = $"3 балла ({percent3:F2}%)" },
+				new PieSlice() { Value = mark2, 
+					FillColor = Colors.Red, Label = $"2 балла ({percent2:F2}%)" },
+				new PieSlice() { Value = mark1, 
+					FillColor = Colors.Red, Label = $"1 балл ({percent1:F2}%)" },
 			};
 
+			// Добавление круга с частями на график
 			var pie = fpGradeAnalysis.Plot.Add.Pie(slices);
+			// Внутренний радиус круга
 			pie.DonutFraction = .5;
-
+			// Показываем подпись частей
 			fpGradeAnalysis.Plot.ShowLegend();
-
+			// Убираем оси X, Y
 			fpGradeAnalysis.Plot.Axes.Frameless();
+			// Убираем квадратную разметку
 			fpGradeAnalysis.Plot.HideGrid();
-
+			// Обновляем график
 			fpGradeAnalysis.Refresh();
+		}
+
+		private void cbFaculty_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			selectedFaculty = (Faculty?)cbFaculty.SelectedItem;
+		}
+
+		private void btnShow_Click(object sender, EventArgs e)
+		{
+			LoadGradeAnalysis();
 		}
 	}
 }
