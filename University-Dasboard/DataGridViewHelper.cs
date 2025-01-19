0bf24bda135f7866785731ef32cc36e3839cd807
Hak1e <hakie.notiko@gmail.com>
2025-01-19 21:24:07 +0400

    The 'acceleration' of the combo box is placed in a separate method

--- University-Dasboard/DataGridViewHelper.cs
+++ University-Dasboard/DataGridViewHelper.cs
@@ -12,6 +12,16 @@
                 }
             }
         }
-    }
+
+		public static void ExpandComboBoxOnEdit(DataGridView dgv, DataGridViewCellEventArgs e)
+		{
+			DataGridViewCell cell = dgv.Rows[e.RowIndex].Cells[e.ColumnIndex];
+			if (cell is DataGridViewComboBoxCell)
+			{
+				dgv.BeginEdit(false);
+				((DataGridViewComboBoxEditingControl)dgv.EditingControl).DroppedDown = true;
+			}
+		}
+	}
 }
 
