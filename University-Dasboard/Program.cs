15b91d1ec5af3014cf2a768f9042661b26c5e0aa
Niki <nikbel2004@outlook.com>
2025-01-21 05:16:31 +0400

    Authorazation

--- University-Dasboard/Program.cs
+++ University-Dasboard/Program.cs
@@ -14,7 +14,7 @@ namespace University_Dasboard
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmMainProgram());
+            Application.Run(new FrmAuthorization());
         }
     }
 }
