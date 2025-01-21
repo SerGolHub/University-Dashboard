dbaebee917f7d764f342fae7e23ae6a150af5991
Hak1e <hakie.notiko@gmail.com>
2025-01-21 20:40:39 +0400

    Added commented line

--- University-Dasboard/Program.cs
+++ University-Dasboard/Program.cs
@@ -15,6 +15,7 @@ namespace University_Dasboard
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new FrmMainProgram());
-        }
-    }
+			//Application.Run(new FrmAuthorization());
+		}
+	}
 }
