fab3d8f00cc62442fc33436732d69182bbf9b71a
Hak1e <hakie.notiko@gmail.com>
2025-01-21 20:39:04 +0400

    Swapped auth form to main

--- University-Dasboard/Program.cs
+++ University-Dasboard/Program.cs
@@ -14,7 +14,7 @@ namespace University_Dasboard
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmAuthorization());
+            Application.Run(new FrmMainProgram());
         }
     }
 }
